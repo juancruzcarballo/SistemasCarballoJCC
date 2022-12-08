8282080e3ec8e19f3a9b490be62ef4c0f14ad497
Juan Cruz Carballo <juancruzcarballo37@gmail.com>
2022-12-08 00:42:27 -0300

    main

---
+++ Tarea3JuanCruzCarballo/ADO/VentaHandler.cs
@@ -0,0 +1,50 @@
+﻿using Tarea3JuanCruzCarballo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Tarea3JuanCruzCarballo.ADO
+{
+    public static class VentaHandler
+    {
+        public static string ConnectionString = "Data Source=DESKTOP-IHFL947; Initial Catalog = carballo_Sistema_Gestion; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public static List<Venta> GetVentas(int id)
+        {
+            List<Venta> ventas = new List<Venta>();
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand())
+                {
+                    sqlCommand.Connection = sqlConnection;
+                    sqlCommand.Connection.Open();
+                    sqlCommand.CommandText = @"select * from venta
+                                                where IdUsuario = @IdUsuario;";
+
+                    sqlCommand.Parameters.AddWithValue("@IdUsuario", id);
+
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                    dataAdapter.SelectCommand = sqlCommand;
+                    DataTable table = new DataTable();
+                    dataAdapter.Fill(table); //Se ejecuta el Select
+
+                    foreach (DataRow row in table.Rows)
+                    {
+                        Venta venta = new Venta();
+                        venta.Id = Convert.ToInt32(row["Id"]);
+                        venta.Comentarios = row["Comentarios"].ToString();
+                        venta.IdUsuario = Convert.ToInt32(row["IdUsuario"]);
+
+                        ventas.Add(venta);
+                    }
+                    sqlCommand.Connection.Close();
+                }
+            }
+            return ventas;
+        }
+
+      
+    }
+}
